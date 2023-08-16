6787a19b8fb3778384d98fcb4139d57ae7522cce
user avadapal <avadapal@uwaterloo.ca>
2023-08-16 09:54:49 -0400

    minor updates

--- repro/generate_raw_data.sh
+++ repro/generate_raw_data.sh
@@ -1,7 +1,7 @@
   
 echo "Running the Heap Extract Experiments (Basic)"
 cd ..
-nitrs=2
+nitrs=10
 for itr in $(seq 1 $nitrs); do
     echo $itr
     echo "heapsize: 16\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_16_itr_$itr"	
@@ -10,35 +10,50 @@ for itr in $(seq 1 $nitrs); do
     echo "heapsize: 16\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_16_itr_$itr"	
     ./docker/run-experiment heap -m 16 -d 16 -i 0 -e 1 -opt 0 -s 0 -itr itr >> "repro/data/log_basic_heap_extract_online_16_itr_$itr"
     echo "preprocessing_heap_16 (basic online) - iteration $itr"
-    
+done    
+  
+nitrs=10
+for itr in $(seq 1 $nitrs); do
     echo "heapsize: 18\nis_optimized: 0\n" > 		            "repro/data/log_basic_heap_extract_preproc_18_itr_$itr"	
     ./docker/run-experiment -p m:71 a:18 s:17 r18:102 c:36 p:128 >> "repro/data/log_basic_heap_extract_preproc_18_itr_$itr"
     echo "preprocessing_heap_18 (basic online)"
     echo "heapsize: 18\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_18_itr_$itr"
     ./docker/run-experiment heap -m 18 -d 18 -i 0 -e 1 -opt 0 -s 0 -itr itr >> "repro/data/log_basic_heap_extract_online_18_itr_$itr"
     echo "preprocessing_heap_18 (basic online)"	
+done
 
+nitrs=10
+for itr in $(seq 1 $nitrs); do
     echo "heapsize: 20\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_20_itr_$itr"	
     ./docker/run-experiment -p m:80 a:20 s:19 r20:114 c:40 p:128 >> "repro/data/log_basic_heap_extract_preproc_20_itr_$itr"
     echo "preprocessing_heap_20 (basic preproc)"
     echo "heapsize: 20\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_20_itr_$itr"
     ./docker/run-experiment -t 8 heap -m 20 -d 20 -i 0 -e 1 -opt 0 -s 0 -itr itr >> "repro/data/log_basic_heap_extract_online_20_itr_$itr"
     echo "preprocessing_heap_20 (basic online)"
+done
 
+nitrs=10
+for itr in $(seq 1 $nitrs); do
     echo "heapsize: 22\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_22_itr_$itr"	
    ./docker/run-experiment -p m:87 a:22 s:21 r22:126 c:44 p:128 >> "repro/data/log_basic_heap_extract_preproc_22_itr_$itr"
    echo "preprocessing_heap_22 (basic preproc)"
    echo "heapsize: 22\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_22_itr_$itr"
    ./docker/run-experiment -t 8 heap -m 22 -d 22 -i 0 -e 1 -opt 0 -s 0 -itr itr >>  "repro/data/log_basic_heap_extract_online_22_itr_$itr"
    echo "preprocessing_heap_22 (basic online)"
+done
 
+nitrs=10
+for itr in $(seq 1 $nitrs); do
   echo "heapsize: 24\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_24_itr_$itr"
   ./docker/run-experiment -p m:69 a:23 s:22 r24:132 c:46 >>  "repro/data/log_basic_heap_extract_preproc_24_itr_$itr"
   echo "preprocessing_heap_24 (basic preproc)"
   echo "heapsize: 24\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_24_itr_$itr"
   ./docker/run-experiment -t 8 heap -m 24 -d 24 -i 0 -e 1 -opt 0 -s 0 -itr itr >>   "repro/data/log_basic_heap_extract_online_24_itr_$itr"
   echo "preprocessing_heap_24 (basic online)"
-  
+done
+
+nitrs=0
+for itr in $(seq 1 $nitrs); do
   echo "heapsize: 26\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_26_itr_$itr"
   ./docker/run-experiment -p m:75 a:25 s:24 r26:100 c:50 p:8 >>  "repro/data/log_basic_heap_extract_preproc_26_itr_$itr"
   echo "heapsize: 26\nis_optimized: 0\n" > "repro/data/logb_basic_heap_extract_preproc_26_itr_$itr"

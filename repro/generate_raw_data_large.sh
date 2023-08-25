f44e788a1c96d6c153d770014a6de84e3e81f4c3
user avadapal <avadapal@uwaterloo.ca>
2023-08-25 08:17:03 -0400

    changed to repro

--- repro/generate_raw_data_large.sh
+++ repro/generate_raw_data_large.sh
@@ -5,10 +5,12 @@ nitrs=2
 for itr in $(seq 1 $nitrs); do
     echo $itr
     echo "heapsize: 28\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_preproc_28_itr_$itr"	
-    ./docker/run-experiment -p m:111 a:28 s:27 r28:81 c:56 p:128 >> "repro/data/log_basic_heap_extract_preproc_28_itr_$itr"
-     echo "heapsize: 28\nis_optimized: 0\n" > "repro/data/log2_basic_heap_extract_preproc_28_itr_$itr"
-    ./docker/run-experiment -a -p r28:81 >> "repro/data/log2_basic_heap_extract_preproc_28_itr_$itr"
+    ./docker/run-experiment -p m:111 a:28 s:27 r28:12 c:56 p:128 >> "repro/data/log_basic_heap_extract_preproc_28_itr_$itr"
     
+    for npre in $(seq 1 15); do
+	 echo "heapsize: 28\nis_optimized: 0\n" > "repro/data/log_"$npre"_basic_heap_extract_preproc_28_itr_$itr"
+    	 ./docker/run-experiment -a -p r28:10 >> "repro/data/log_"$npre"_basic_heap_extract_preproc_28_itr_$itr"
+     done 
     echo "preprocessing_heap_28 (basic preproc) - iteration $itr"
     echo "heapsize: 28\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_28_itr_$itr"	
     ./docker/run-experiment heap -m 28 -d 28 -i 0 -e 1 -opt 0 -s 0 -itr itr >> "repro/data/log_basic_heap_extract_online_28_itr_$itr"
@@ -18,13 +20,13 @@ done
 nitrs=2
 for itr in $(seq 1 $nitrs); do
     echo "heapsize: 30\nis_optimized: 0\n" > 		            "repro/data/log_basic_heap_extract_preproc_30_itr_$itr"	
-    ./docker/run-experiment -p m:119 a:30 s:29 r30:87 c:60 p:128 >> "repro/data/log_basic_heap_extract_preproc_30_itr_$itr"
+    ./docker/run-experiment -p m:119 a:30 s:29 r30:17 c:60 p:128 >> "repro/data/log_basic_heap_extract_preproc_30_itr_$itr"
     echo "preprocessing_heap_30 (basic online)"
-    
+    for npre in $(seq 1 15); do
     echo "heapsize: 30\nis_optimized: 0\n" >                        "repro/data/log3_basic_heap_extract_preproc_30_itr_$itr"
-    ./docker/run-experiment -a -p r30:87  >> "repro/data/log2_basic_heap_extract_preproc_30_itr_$itr"
+    ./docker/run-experiment -a -p r30:10  >> "repro/data/log_"$npre"_basic_heap_extract_preproc_30_itr_$itr"
      echo "preprocessing_heap_30 (basic online)"
-
+	done
     echo "heapsize: 30\nis_optimized: 0\n" > "repro/data/log_basic_heap_extract_online_30_itr_$itr"
     ./docker/run-experiment heap -m 30 -d 30 -i 0 -e 1 -opt 0 -s 0 -itr itr >> "repro/data/log_basic_heap_extract_online_30_itr_$itr"
     echo "preprocessing_heap_30 (basic online)"	
